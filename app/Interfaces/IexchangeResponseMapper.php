881791a5a88148b5ed8f4b184f97174bb7feb013
MohsenCreative <mohsen.aero72@gmail.com>
2024-12-21 01:44:44 +0330

    add comment to interface ResponseMapper

--- app/Interfaces/IexchangeResponseMapper.php
+++ app/Interfaces/IexchangeResponseMapper.php
@@ -11,7 +11,10 @@ namespace App\Interfaces;
 interface IexchangeResponseMapper{
 
 
-
+    /**
+     * @param object $response
+     * @return array
+     */
  public function mapOrderResponse(object $response) : array;
 
 
